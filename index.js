5e97f96c09eb20dd92fe6c76d529ca2a30ac8c11
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-13 23:24:36 +0700

    chore: meta tweaks

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const path = require('path');
+const { dirname } = require('path');
 const findUp = require('find-up');
 
 const gitRootDir = (cwd = process.cwd()) =>
   findUp('.git', { cwd }).then(
-    filePath => (filePath ? path.dirname(filePath) : null)
+    filepath => (filepath ? dirname(filepath) : null)
   );
 
 module.exports = gitRootDir;
