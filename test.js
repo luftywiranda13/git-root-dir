a67e4f09d090d90742fe8551c3275b6150767d56
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-03-29 20:49:11 +0700

    chore(package): update xo to version 0.20.3 (#5)
    * chore(package): update xo to version 0.20.3
    
    Closes #3
    
    * refactor: adapt changes to the latest `xo`

--- test.js
+++ test.js
@@ -2,24 +2,22 @@
 
 const { homedir } = require('os');
 
-const gitRootDir = require('./');
+const gitRootDir = require('.');
 
-describe('find `.git` dir', () => {
-  it('can find `.git` dir from `fixtures`', async () => {
-    expect.assertions(1);
+it('can find `.git` dir from `fixtures`', async () => {
+  expect.assertions(1);
 
-    await expect(gitRootDir('fixtures')).resolves.toBe(__dirname);
-  });
+  await expect(gitRootDir('fixtures')).resolves.toBe(__dirname);
+});
 
-  it('can find `.git` dir from `process.cwd()`', async () => {
-    expect.assertions(1);
+it('can find `.git` dir from `process.cwd()`', async () => {
+  expect.assertions(1);
 
-    await expect(gitRootDir()).resolves.toBe(__dirname);
-  });
+  await expect(gitRootDir()).resolves.toBe(__dirname);
+});
 
-  it("can't find `.git` dir from `homedir()`", async () => {
-    expect.assertions(1);
+it("can't find `.git` dir from `homedir()`", async () => {
+  expect.assertions(1);
 
-    await expect(gitRootDir(homedir())).resolves.toBe(null);
-  });
+  await expect(gitRootDir(homedir())).resolves.toBe(null);
 });
